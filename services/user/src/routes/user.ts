5b6e43b2d533c108775dd252d368384f7d1b61b8
Swarnavo2003 <swarnavo2020@gmail.com>
2026-02-19 17:32:10 +0530

    feat: add and delete skill for user endpoints added

--- services/user/src/routes/user.ts
+++ services/user/src/routes/user.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { isAuth } from "../middleware/auth.js";
 import {
+  addSkillToUser,
+  deleteSkillFromUser,
   getUserProfile,
   myProfile,
   updateProfilePic,
@@ -16,5 +18,7 @@ router.get("/:userId", isAuth, getUserProfile);
 router.put("/update/profile", isAuth, updateUserProfile);
 router.put("/update/pic", isAuth, uploadFile, updateProfilePic);
 router.put("/update/resume", isAuth, uploadFile, updateResume);
+router.post("/skill/add", isAuth, addSkillToUser);
+router.delete("/skill/delete", isAuth, deleteSkillFromUser);
 
 export default router;
