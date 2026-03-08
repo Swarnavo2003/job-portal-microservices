1064c6fdb4458172296309212dcdfce5b74baedb
Swarnavo2003 <swarnavo2020@gmail.com>
2026-03-08 16:21:26 +0530

    ai powered career recommendation section added

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,6 +26,7 @@ export default function RootLayout({
           <div>
             <Navbar />
             {children}
+            <Toaster />
           </div>
         </ThemeProvider>
       </body>
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
+import { CareerGuide } from "@/components/career-guide";
 import { Hero } from "@/components/hero";
 
 export default function Home() {
   return (
     <div className="p-2 space-y-2">
       <Hero />
+      <CareerGuide />
     </div>
   );
 }
