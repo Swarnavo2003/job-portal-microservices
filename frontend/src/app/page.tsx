397315ea734b4a979f8d6f46931bdb3806318684
Swarnavo2003 <swarnavo2020@gmail.com>
2026-03-09 11:27:30 +0530

    feat: resume analyzer section and about page added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,13 @@
 import { CareerGuide } from "@/components/career-guide";
 import { Hero } from "@/components/hero";
+import { ResumeAnalyzer } from "@/components/resume-analyser";
 
 export default function Home() {
   return (
     <div className="p-2 space-y-2">
       <Hero />
       <CareerGuide />
+      <ResumeAnalyzer />
     </div>
   );
 }
